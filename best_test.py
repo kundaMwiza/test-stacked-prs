5faa2eba57ef91b1869f397c805ed2ecd0b63f50
Mwiza Kunda <mwizakunda@gmail.com>
2024-08-24 11:03:04 +0100

    Add the best add sub test
    ghstack-source-id: d755e217bd5459dde010d023468628024aab7410
    Pull Request resolved: https://github.com/kundaMwiza/test-stacked-prs/pull/2

--- best_test.py
+++ best_test.py
@@ -16,3 +16,9 @@ def test_add(input_a, input_b) -> None:
 def test_sub(input_a, input_b) -> None:
     result = input_a - input_b
     assert (result != 0).all()
+
+def test_add_sub(input_a, input_b) -> None:
+    result_add = input_a + input_b 
+    result_sub = input_a - input_b
+    result = result_add + result_sub
+    assert (result != 0).all()
