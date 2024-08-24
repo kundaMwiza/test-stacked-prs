a9e37e327567d13c04d5ac22e5933d9ed3ca2b06
Mwiza Kunda <mwizakunda@gmail.com>
2024-08-24 13:17:50 +0100

    Add fibonacci test
    ghstack-source-id: d34f4310ba82dfbce4208fffa53a84e49e5a8851
    Pull Request resolved: https://github.com/kundaMwiza/test-stacked-prs/pull/4

--- second_best_test.py
+++ second_best_test.py
@@ -3,10 +3,22 @@ import functools
 
 @functools.lru_cache
 def factorial(n):
+    assert isinstance(n, int) and n >= 0, f"{n=} must be an integer that is atleast 0"
     if n == 1 or n == 0:
         return 1
     return  n * factorial(n-1)
 
+@functools.lru_cache
+def fibonacci(n):
+    assert isinstance(n, int) and n >= 0, f"{n=} must be an integer that is atleast 0"
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
 def test_factorial():
     assert factorial(5) == 120
 
+def test_fibonacci():
+    assert fibonacci(9) == 34
